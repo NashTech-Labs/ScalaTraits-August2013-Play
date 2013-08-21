86548450be8de5b94135fb653d20dc7e489dfdce
neelsachdeva <neelkanth@knoldus.com>
2013-08-21 16:47:31 +0530

    WS , XML , JSON Demo

--- MyOffice/app/controllers/Application.scala
+++ MyOffice/app/controllers/Application.scala
@@ -2,11 +2,52 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-
+import play.api.libs.ws.WS
+import play.api.libs.concurrent.Execution.Implicits._
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  /**
+   * Calling web services
+   */
+
+  def jobs = Action {
+    WS.url("http://www.scalajobz.com/getAllJobs").get().map {
+      response => println(response.json)
+    }
+    Ok("Jobs Fetched")
+  }
+
+  /**
+   * XML Demo
+   */
+  def sayHello = Action { request =>
+    request.body.asXml.map { xml =>
+      (xml \\ "name" headOption).map(_.text).map { name =>
+        Ok("Hello " + name + "   ")
+      }.getOrElse {
+        BadRequest("Missing parameter [name]")
+      }
+    }.getOrElse {
+      BadRequest("Expecting Xml data")
+    }
+  }
+
+  /**
+   * Json Demo
+   */
+  def greet = Action { request =>
+    request.body.asJson.map { json =>
+      (json \ "name").asOpt[String].map { name =>
+        Ok("Hello " + name + " ")
+      }.getOrElse {
+        BadRequest("Missing parameter [name]")
+      }
+    }.getOrElse {
+      BadRequest("Expecting Json data")
+    }
+  }
 }
